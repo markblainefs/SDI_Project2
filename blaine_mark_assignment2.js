75ba09521e748af07612880e1f82d5da6decefe9
Mark Blaine <markopollo@Marks-MacBook-Pro.local>
2012-06-08 17:06:55 -0500

    Added howDoWeGetThere function

--- blaine_mark_assignment2.js
+++ blaine_mark_assignment2.js
@@ -1,1 +1,24 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//Mark A. Blaine;
+//SDI 1206;
+//Assignment 2;
+
+	var bandsPlaying ["Ludacris", "Red Hot Chili Peppers", "Radiohead"];
+	var startTime = 1800;
+	var stageName = "Where";
+	var transportaion = "car";
+	var doIHaveATicket = true;
+	
+	var howDoWeGetThere = function(){
+		if (transportation === "car"){
+			console.log("We are driving to Tennessee");
+		} else {
+			console.log("We need to find a way to Bonnaroo");
+		};
+	}
+	
+	var canIGoToBonaroo = function(){
+		if (doIHaveATicket)
+		
+	}
